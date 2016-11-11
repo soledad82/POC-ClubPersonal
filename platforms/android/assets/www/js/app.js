964cfc9e95112676112e9f575b6eab6eda82f2bc
Diego Sanz <diego.sanz@neoris.com>
2016-11-11 18:23:31 -0300

    delete platform

--- platforms/android/assets/www/js/app.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('app', []);
-myApp.controller('SplitterController', function() {
-        this.load = function(page) {
-            mySplitter.content.load(page)
-                .then(function() {
-                    mySplitter.left.close();
-                });
-        };
-    });
